e56823c33ba5cdaf49fd87a589172e4538ea3cb0
changwoo <ep6tri@hotmail.com>
2026-04-08 04:13:27 +0900

    코드 커밋

--- src/App.php
+++ src/App.php
@@ -4,23 +4,32 @@ namespace Chwnam\TRC;
 
 class App
 {
+    const int REST = 30;
+
     public function run(): void
     {
         $config  = Config::init();
         $cursor  = new Cursor($config);
         $dumper  = new Dumper($config, $cursor);
         $fetcher = new RepliesFetcher($config, $cursor);
+        $count = 0;
 
         while(1) {
             $fetched = $fetcher->fetch();
             if (empty($fetched)) {
                 break;
             }
-            echo "Fetched page " . $cursor->count . "\n";
+            echo "Fetched page " . $cursor->count . ".\n";
             $dumper->dump($fetched);
             $cursor->saveState();
-            sleep(3);
+            sleep($config->sleepTime);
+
+            if (0 === (++$count % self::REST)) {
+                echo "Sleeping for 30 seconds...\n";
+                sleep(30);
+            }
         }
+
         echo "Done!\n";
     }
 }
--- src/Config.php
+++ src/Config.php
@@ -9,21 +9,23 @@ readonly final class Config
     public string $accessToken;
     public string $userId;
     public string $dumpPath;
+    public int $sleepTime;
 
-    public function __construct()
+    private function __construct()
     {
         // loads the .env file
         $dotenv = Dotenv::createImmutable(ROOT_DIR);
         $dotenv->load();
         $dotenv
-            ->required(['ACCESS_TOKEN', 'USER_ID', 'DUMP_PATH'])
+            ->required(['ACCESS_TOKEN', 'USER_ID', 'DUMP_PATH', 'SLEEP_TIME'])
             ->notEmpty();
 
         $this->accessToken = $_ENV['ACCESS_TOKEN'];
         $this->userId      = $_ENV['USER_ID'];
         $this->dumpPath    = $_ENV['DUMP_PATH'];
+        $this->sleepTime   = (int)abs($_ENV['SLEEP_TIME']) ?: 5;
 
-        if (!file_exists($this->dumpPath) || !is_dir($this->dumpPath) || !is_writable($this->dumpPath)) {
+        if ( ! file_exists($this->dumpPath) || ! is_dir($this->dumpPath) || ! is_writable($this->dumpPath)) {
             die('DUMP_PATH is not a valid directory. It should be a writable directory.');
         }
     }
