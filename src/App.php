9865119c41a6ef608db6e09183bf122b246078a8
changwoo <ep6tri@hotmail.com>
2026-04-08 09:54:48 +0900

    코드 커밋

--- src/App.php
+++ src/App.php
@@ -2,6 +2,8 @@
 
 namespace Chwnam\TRC;
 
+use Exception;
+
 class App
 {
     const int REST = 30;
@@ -12,21 +14,30 @@ class App
         $cursor  = new Cursor($config);
         $dumper  = new Dumper($config, $cursor);
         $fetcher = new RepliesFetcher($config, $cursor);
-        $count = 0;
+        $count   = 0;
 
-        while(1) {
-            $fetched = $fetcher->fetch();
+        while (1) {
+            try {
+                $fetched = $fetcher->fetch();
+            } catch (Exception $e) {
+                echo $e->getMessage() . "\n" . "Sleeping for 30 seconds...\n";
+                sleep(30);
+                continue;
+            }
+
             if (empty($fetched)) {
                 break;
             }
+
             echo "Fetched page " . $cursor->count . ".\n";
             $dumper->dump($fetched);
             $cursor->saveState();
-            sleep($config->sleepTime);
 
             if (0 === (++$count % self::REST)) {
                 echo "Sleeping for 30 seconds...\n";
                 sleep(30);
+            } else {
+                sleep($config->sleepTime);
             }
         }
 
